a140ed8052e7ebdd861fc736da60af7b0c04a451
angryz <angryzorro@gmail.com>
2016-08-19 17:18:20 +0800

    Practice RDD operation

---
+++ src/main/java/practice/spark/hello/RddOperation.java
@@ -0,0 +1,23 @@
+package practice.spark.hello;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+
+/**
+ * @author Zheng Zhipeng
+ */
+public class RddOperation {
+
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("My App");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<Integer> rdd = sc.parallelize(Arrays.asList(1, 2, 3, 4));
+        JavaRDD<Integer> result = rdd.map(x -> x * x);
+        System.out.println(StringUtils.join(result.collect(), ","));
+    }
+}
