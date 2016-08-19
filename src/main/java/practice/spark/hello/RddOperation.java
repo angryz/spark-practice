911dab4c0368424becb7d44c1436eb108786e986
angryz <angryzorro@gmail.com>
2016-08-19 17:34:04 +0800

    Practice flatMap() operation

--- src/main/java/practice/spark/hello/RddOperation.java
+++ src/main/java/practice/spark/hello/RddOperation.java
@@ -16,8 +16,14 @@ public class RddOperation {
         SparkConf conf = new SparkConf().setMaster("local").setAppName("My App");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
+        // map() example
         JavaRDD<Integer> rdd = sc.parallelize(Arrays.asList(1, 2, 3, 4));
         JavaRDD<Integer> result = rdd.map(x -> x * x);
         System.out.println(StringUtils.join(result.collect(), ","));
+
+        // flatMap() example
+        JavaRDD<String> lines = sc.parallelize(Arrays.asList("hello world", "hi"));
+        JavaRDD<String> words = lines.flatMap(line -> Arrays.asList(line.split("\\s")));
+        System.out.println(words.first());
     }
 }
