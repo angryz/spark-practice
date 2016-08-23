fd8d2c23ff4ab3ac24c838e4022fa1b7d19ac4e2
angryz <angryzorro@gmail.com>
2016-08-23 16:17:16 +0800

    Practice pair RDD

--- src/main/java/practice/spark/hello/PairRddBasic.java
+++ src/main/java/practice/spark/hello/PairRddBasic.java
@@ -18,13 +18,18 @@ import java.util.Arrays;
 public class PairRddBasic {
 
     public static void main(String[] args) {
-        // 使用每一行的第一个单词创建一个 pair RDD
         SparkConf conf = new SparkConf().setMaster("local").setAppName("My App");
         JavaSparkContext sc = new JavaSparkContext(conf);
+
+        // 使用每一行的第一个单词创建一个 pair RDD
         JavaRDD<String> lines = sc.parallelize(Arrays.asList("hello world", "foo bar", "good job"));
         PairFunction<String, String, String> keyData =
                 (PairFunction<String, String, String>) s -> new Tuple2<>(s.split(" ")[0], s);
         JavaPairRDD<String, String> pairs = lines.mapToPair(keyData);
         System.out.println(StringUtils.join(pairs.collect(), ", "));
+
+        // 筛选掉长度超过10个字符的行
+        JavaPairRDD<String, String> result = pairs.filter(kv -> (kv._2().length() < 10));
+        System.out.println(StringUtils.join(result.collect(), ", "));
     }
 }
