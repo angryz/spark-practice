d9b6158bf0fea3358f461d0593e24e43d10bb3ed
angryz <angryzorro@gmail.com>
2016-08-23 17:34:18 +0800

    Practice pair RDD operations

--- src/main/java/practice/spark/hello/PairRddOperation.java
+++ src/main/java/practice/spark/hello/PairRddOperation.java
@@ -3,6 +3,7 @@ package practice.spark.hello;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
@@ -21,6 +22,7 @@ public class PairRddOperation {
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         // mapValues(), reduceByKey()
+        // 计算各个 key 的平均值
         JavaPairRDD<String, Integer> numbers = sc.parallelizePairs(Arrays.asList(
                 new Tuple2<>("apple", 1),
                 new Tuple2<>("banana", 2),
@@ -38,6 +40,16 @@ public class PairRddOperation {
         System.out.println(StringUtils.join(reduced.collect(), ", "));
         JavaPairRDD<String, Double> avg = reduced.mapValues(v1 -> v1.avg());
         System.out.println("Avg : " + StringUtils.join(avg.collect(), ", "));
+
+        // 计算单词数
+        JavaRDD<String> input = sc.textFile("src/main/resources/spark-readme.md");
+        JavaRDD<String> words = input.flatMap(x -> Arrays.asList(x.split("\\s")));
+        JavaPairRDD<String, Integer> result = words
+                .mapToPair(s -> new Tuple2<String, Integer>(s, 1))
+                .reduceByKey((v1, v2) -> v1 + v2);
+        System.out.println("Words count: " + result.takeSample(true, 20));
+        // faster way to do the same
+        //input.flatMap(x -> Arrays.asList(x.split("\\s"))).countByValue();
     }
 
     private static class AvgCount implements Serializable {
