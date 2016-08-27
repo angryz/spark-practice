7099e1d05d8efc8e34de6dc446a8a7a88aa02135
angryz <angryzorro@gmail.com>
2016-08-27 17:54:58 +0800

    Fix sortByKey()

--- src/main/java/practice/spark/hello/SortByKeyDemo.java
+++ src/main/java/practice/spark/hello/SortByKeyDemo.java
@@ -1,11 +1,18 @@
 package practice.spark.hello;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Int;
 import scala.Tuple2;
 
+import java.io.Serializable;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,22 +24,36 @@ public class SortByKeyDemo {
         SparkConf conf = new SparkConf().setMaster("local").setAppName("my app");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaPairRDD<Integer, String> pairs = sc.parallelizePairs(Arrays.asList(
-                new Tuple2<>(1, "apple"),
                 new Tuple2<>(2, "banana"),
-                new Tuple2<>(3, "cherry"),
+                new Tuple2<>(1, "apple"),
                 new Tuple2<>(4, "durian"),
-                new Tuple2<>(5, "fig"),
+                new Tuple2<>(3, "cherry"),
                 new Tuple2<>(6, "grape"),
+                new Tuple2<>(5, "fig"),
                 new Tuple2<>(7, "haw")
         ));
 
-        JavaPairRDD result = pairs.sortByKey(
-                (a, b) -> Integer.toString(a).compareTo(Integer.toString(b)),
-                false
-        );
-        Map<Integer, String> resultMap = result.collectAsMap();
-        for (Map.Entry<Integer, String> entry : resultMap.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
+        JavaPairRDD<Integer, String> result1 = pairs.sortByKey(false);
+        List<Tuple2<Integer, String>> list1 = result1.collect();
+        for (Tuple2<Integer, String> x : list1) {
+            System.out.println(x._1() + " : " + x._2());
+        }
+
+        JavaPairRDD<Integer, String> result2 = pairs.sortByKey(new IntegerComparator(), false);
+        List<Tuple2<Integer, String>> list2 = result2.collect();
+        for (Tuple2<Integer, String> x : list2) {
+            System.out.println(x._1() + " : " + x._2());
+        }
+    }
+
+    // this must implements Serializable, or else exception will be throwed.
+    static class IntegerComparator implements Comparator<Integer>, Serializable {
+
+        @Override
+        public int compare(Integer a, Integer b) {
+            String c = String.valueOf(a);
+            String d = String.valueOf(b);
+            return c.compareTo(d);
         }
     }
 }
